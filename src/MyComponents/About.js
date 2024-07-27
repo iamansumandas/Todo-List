fc5347755955fbf91ac3baedc713ec7e8361f152
iamansumandas <emailofansumandas@gmail.com>
2024-07-27 08:23:04 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            About TodoApp 
+            <p>Welcome to TodoApp! Your ultimate solution for managing tasks and staying organized. Whether you're juggling personal errands, managing work projects, or planning your next big event, TodoApp is here to help you keep track of everything in one convenient place.</p>
+        </div>
+    )
+}
